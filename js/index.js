41b94cdd9ba2cda87b549be58c51f7d370ded41f
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-09-27 09:13:32 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -8,6 +8,6 @@ allLi.forEach( (li, index) => {
         li.classList.add("active-list")
 
         const indicator = document.querySelector(".indicator")
-        indicator.style.transform = `translateX(calc(${index * 90}px))`
+        indicator.style.transform = `translateX(calc(${index * 80}px))`
     })
 })
